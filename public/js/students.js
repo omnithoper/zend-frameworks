582a4c3e4a6b575392f93f53227f838f09477983
cesar sanglitan <cesar.sanglitan@englishcentral.com>
2017-02-19 01:32:39 +0800

    ajaxedit

--- public/js/students.js
+++ public/js/students.js
@@ -16,5 +16,25 @@ var viewDetails = function(studentID) {
 		}
 	});
 }
+
+var editStudent = function(studentID)
+{
+	$.ajax({
+		url: '/students/details', 
+		data: {studentID: studentID},
+		contentType: 'application/json; charset=utf-8',
+		success: function(response){
+			console.log('response');
+			console.log(response);
+			console.log(response.first_name);
+			response = $.parseJSON(response);
+			console.log(response);
+			console.log(response.first_name);
+			$('#student-edit #studenID').html(response.student_id);
+			$('#student-edit #unang_pangalan').val(response.first_name);
+			$('#student-edit #apilido').val(response.last_name);
+		}
+	});
+}
 document.onload = function(){
 };
