25e6ca15741061628c9a14a3a2d1561a769bac20
cesar sanglitan <cesar.sanglitan@englishcentral.com>
2017-03-18 22:50:58 +0800

    edit test controller

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -1,10 +1,15 @@
 <?php
-class BaseController extends Zend_Controller_Action {
-	public function render($template) {
+class TestController extends Zend_Controller_Action {
+	public function indexAction() {
+		die('end');
+	}
+	
+	public function fbAction() {
 		$fb = new Facebook\Facebook([
 		  'app_id' => '{app-id}',
 		  'app_secret' => '{app-secret}',
 		  'default_graph_version' => 'v2.5',
 		]);
+		die('end');
 	}
 }
