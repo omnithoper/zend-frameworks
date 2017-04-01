7cab3c328908b387a07e0ef513207d429fa0cbe5
cesar sanglitan <cesar.sanglitan@englishcentral.com>
2017-04-01 23:27:20 +0800

    facebook separation

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -1,12 +1,66 @@
 <?php
 class LoginController extends Zend_Controller_Action {
+	protected $_fb;
+	
 	public function indexAction() {
-			//Include Google client library 
+		$loginType = Request::getParam('loginType');
+
+		$this->__setupFacebookCredentials();
+		$this->__setupGoogleCredentials();
+
+		$this->__getFacebookURL();
+		$this->__getGoogleURL();
+
+
+		if (!empty($loginType)) {
+			call_user_method('__'.$loginType.'Login', $this);
+		}
+	}
+
+	protected function __setupFacebookCredentials() {
+		if (!empty($this->_fb)) {
+			return true;
+		}
+		
+		date_default_timezone_set('America/Los_Angeles');
+
+		$this->_fb = new Facebook\Facebook([
+		  'app_id' => '1729933903964760',
+		  'app_secret' => 'af7d04a68993ac028425a4daa3c154a4',
+		  'default_graph_version' => 'v2.5',
+		]);		
+	}
+
+	protected function __setupGoogleCredentials() {
+	}
+
+	protected function __getFacebookURL() {
+		if (!empty($_SESSION['facebook_access_token'])) {
+			return false;
+		}
+
+		$url = 'http://sample.enrollment.com/?loginType=facebook';
+		$helper = $this->_fb->getRedirectLoginHelper();
+		$permissions = ['email', 'user_likes']; // optional
+		try {
+			$loginUrl = $helper->getLoginUrl($url, $permissions);
+		} catch (Exception $e) {
+			Zend_Debug::dump($e->getMessage()); die();
+		}
+
+		$this->view->fbloginurl = $loginUrl;
+	}
+
+	protected function __getGoogleURL() {
+		
+	}
+
+	protected function __googleLogin() {
+		die('google this');
+		//Include Google client library 
 		require_once 'google/Google_Client.php';
 		require_once 'google/contrib/Google_Oauth2Service.php';
 		
-
-
 		/*
 		 * Configuration and setup Google API
 		 */
@@ -38,8 +92,7 @@ class LoginController extends Zend_Controller_Action {
 			$gpUserProfile = $google_oauthV2->userinfo->get();
 			
 			//Initialize User class
-		
-			
+					
 			//Insert or update user data to the database
 		    $gpUserData = array(
 		        'oauth_provider'=> 'google',
@@ -52,13 +105,11 @@ class LoginController extends Zend_Controller_Action {
 		       // 'picture'       => $gpUserProfile['picture'],
 		        //'link'          => $gpUserProfile['link']
 		    );
-
-		
-			
 		} else {	
 			$authUrl = $gClient->createAuthUrl();
 			echo '<a href="'.filter_var($authUrl, FILTER_SANITIZE_URL).'">login in gmail</a>';
 		}
+		
 		if (!empty($gpUserData)) {
 			$data = array(
 				'google_id' => $gpUserData['id'],  
@@ -73,94 +124,72 @@ class LoginController extends Zend_Controller_Action {
 			$_SESSION['student_id'] = $studentID;
 
 		}	
+		
 		if (!empty($gpUserData)){
 		   		$_SESSION['login_user'] = $gpUserData['first_name'];
   				$_SESSION['user_type'] = 'student';
+				$this->_redirect('/index');
 		}
+	}
 
-	if (empty($accessToken)) { 
-	//this is for fb login
-		date_default_timezone_set('America/Los_Angeles');
-		$url = 'http://sample.enrollment.com/';
-
-		$fb = new Facebook\Facebook([
-		  'app_id' => '1729933903964760',
-		  'app_secret' => 'af7d04a68993ac028425a4daa3c154a4',
-		  'default_graph_version' => 'v2.5',
-
-		]);
-
-		// step 2
-		if (empty($_SESSION['facebook_access_token'])) {	 
-			$helper = $fb->getRedirectLoginHelper();
- 			if(isset($_GET['state'])) {
-     			if($_SESSION['FBRLH_' . 'state']) {
-        			  $_SESSION['FBRLH_' . 'state'] = $_GET['state'];
-      			}
-			}
-
-			try {
-	  			$accessToken = $helper->getAccessToken();
-
-				if (!empty($accessToken)) {
-					$_SESSION['facebook_access_token'] = (string)$accessToken;
-					try {
-						$fields = [
-							'first_name',
-						];
-						$response = $fb->get('/me?fields='.join(',', $fields), $_SESSION['facebook_access_token']);
-						$user = $response->getGraphUser();
-						$_SESSION['login_user'] = $user['name'];
-			  			$_SESSION['user_type'] = 'student';
-	  			
-	  					$student = new Student();
-	  					$_SESSION['student_id'] = $student->facebookStudentExist($user['id']);
-						$this->_redirect('/index');
-						return;
-					} catch(Facebook\Exceptions\FacebookSDKException $e) {
-					  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-					  exit;
-					}
-				}
-			} catch(Facebook\Exceptions\FacebookResponseException $e) {
-	 		 // When Graph returns an error
-	  		echo 'Graph returned an error: ' . $e->getMessage();
-	  		exit;
-			} catch(Facebook\Exceptions\FacebookSDKException $e) {
-	  		// When validation fails or other local issues
-	 		 	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-	  			exit;
-			}
-		}
-
-		// step 1
-		if (empty($_SESSION['facebook_access_token'])) {
-			$helper = $fb->getRedirectLoginHelper();
-			$permissions = ['email', 'user_likes']; // optional
-			try {
-				$loginUrl = $helper->getLoginUrl($url, $permissions);
-			} catch (Exception $e) {
-				Zend_Debug::dump($e->getMessage()); die();
-			}
-
-		//	echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';	
-
-			$this->view->fbloginurl = $loginUrl;
-		}
-
+	protected function __facebookLogin() {
 		if (!empty($_SESSION['facebook_access_token'])) {
 			$fields = [
 				'first_name',
 			];
-			$response = $fb->get('/me?fields='.join(',', $fields), $_SESSION['facebook_access_token']);
+			$response = $this->_fb->get('/me?fields='.join(',', $fields), $_SESSION['facebook_access_token']);
 			$user = $response->getGraphUser();
 			$_SESSION['login_user'] = $user['first_name'];
   			$_SESSION['user_type'] = 'student';
+
+	  					//die('logged in 2');
 			$this->_redirect('/index');
 		}
-	}
 
+		$helper = $this->_fb->getRedirectLoginHelper();
+			if(isset($_GET['state'])) {
+ 			if($_SESSION['FBRLH_' . 'state']) {
+    			  $_SESSION['FBRLH_' . 'state'] = $_GET['state'];
+  			}
+		}
+
+		try {
+  			$accessToken = $helper->getAccessToken();
+
+			if (!empty($accessToken)) {
+				$_SESSION['facebook_access_token'] = (string)$accessToken;
+				try {
+					$fields = [
+						'first_name',
+					];
+					$response = $this->_fb->get('/me?fields='.join(',', $fields), $_SESSION['facebook_access_token']);
+					$user = $response->getGraphUser();
+					#Zend_Debug::dump($user);
+					#die();
+					$_SESSION['login_user'] = $user['first_name'];
+		  			$_SESSION['user_type'] = 'student';
+  			
+  					$student = new Student();
+  					$_SESSION['student_id'] = $student->facebookStudentExist($user['id']);
+  					// die('logged in 1');
+					$this->_redirect('/index');
+					return;
+				} catch(Facebook\Exceptions\FacebookSDKException $e) {
+				  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+				  exit;
+				}
+			}
+		} catch(Facebook\Exceptions\FacebookResponseException $e) {
+ 		 // When Graph returns an error
+  		echo 'Graph returned an error: ' . $e->getMessage();
+  		exit;
+		} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  		// When validation fails or other local issues
+ 		 	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  			exit;
+		}
 	}
+	
 	public function loginAction() {		
 	    $userName = Request::getParam('username');
 	    $password = Request::getParam('password'); 
@@ -180,13 +209,11 @@ class LoginController extends Zend_Controller_Action {
 	}
 	
 	public function logoutAction() {
-		if (!empty($_SESSION['login_user'])) {
-			unset($_SESSION['login_user']);
-			unset($_SESSION['user_type']);
-			unset($_SESSION['student_id']);
-			unset($_SESSION['facebook_access_token']);
-			unset($_SESSION['google_access_token']);
-		}
+		unset($_SESSION['login_user']);
+		unset($_SESSION['user_type']);
+		unset($_SESSION['student_id']);
+		unset($_SESSION['facebook_access_token']);
+		unset($_SESSION['google_access_token']);
 		session_write_close();
 	    header('Location: /');
 	}
